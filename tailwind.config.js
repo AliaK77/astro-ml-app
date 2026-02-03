76d35383e3b95ba28fb5094337d948555bd73cbf
AliaK77 <aliak77@gmail.com>
2026-02-04 00:04:28 +1100

    Update tailwind.config.js
    reduce fontsizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontSize: {
+        'xs': ['0.6rem', { lineHeight: '0.8rem' }],    // 80% of default
+        'sm': ['0.7rem', { lineHeight: '1rem' }],
+        'base': ['0.8rem', { lineHeight: '1.2rem' }],
+        'lg': ['0.9rem', { lineHeight: '1.4rem' }],
+        'xl': ['1rem', { lineHeight: '1.5rem' }],
+        '2xl': ['1.2rem', { lineHeight: '1.8rem' }],
+        '3xl': ['1.5rem', { lineHeight: '2rem' }],
+        '4xl': ['1.8rem', { lineHeight: '2.25rem' }],
+      },
+    },
+  },
+  plugins: [],
 }
-
